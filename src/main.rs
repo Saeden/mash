0663592f2dc0bd58a71e10c52ed4c4fec4204ce2
Matthijs Wolters <matthijs.wolters@proton.me>
2024-08-31 14:12:59 +0200

    Add console dep and readline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use console::Term;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let term = Term::stdout();
+        let line = Term::read_line(&term).unwrap();
+        let parsed_line = parse_line(line);
+    }
+}
+
+fn parse_line(line: String) -> String {
+    println!("{}", line);
+    line
 }
