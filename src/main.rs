8aff49c5a946657f537d65c6554240f7afde2fac
Matthijs Wolters <matthijs.wolters@proton.me>
2024-09-02 22:27:45 +0200

    Add tokenize && remove dep console

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use console::Term;
-
 fn main() {
     loop {
-        let term = Term::stdout();
-        let line = Term::read_line(&term).unwrap();
-        let parsed_line = parse_line(line);
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let args = tokenize(line);
+        println!("{:?}", args);
     }
 }
 
-fn parse_line(line: String) -> String {
-    println!("{}", line);
-    line
+fn tokenize(line: String) -> Vec<String> {
+    let mut buffer: Vec<char> = Vec::new();
+    let mut tokens: Vec<String> = Vec::new();
+    for c in line.chars() {
+        if c.is_whitespace() && !buffer.is_empty() {
+            tokens.push(buffer.iter().collect());
+            Vec::clear(&mut buffer);
+        } else if !c.is_whitespace() {
+            buffer.push(c);
+        }
+    }
+    if !buffer.is_empty() {
+        tokens.push(buffer.iter().collect());
+    }
+    tokens
 }
